2b20d883e83342e7d623b0cda264a1111abd88d7
Singh Pushkar Surendra <144793225+pushkar-web@users.noreply.github.com>
2025-06-08 10:36:01 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -54,7 +54,7 @@ export const Footer = () => {
             rel="noreferrer noopener"
             className="text-purple"
           >
-            Sanidhya
+            Pushkar Singh
           </Link>{" "}
           |{" "}
           <Link href={links.sourceCode} className="underline">
